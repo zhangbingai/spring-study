a8238fc863506bf6c9e5558e3fd56c9813ead388
bing.zhang <Bing.zhang@trjcn.com>
2020-06-19 10:26:24 +0800

    add eureka client

--- payment/src/main/java/com/bing/springcloud/PaymentApplication.java
+++ payment/src/main/java/com/bing/springcloud/PaymentApplication.java
@@ -2,6 +2,7 @@ package com.bing.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author bing.zhang
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/6/17 10:27
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentApplication {
 
     public static void main(String[] args) {
