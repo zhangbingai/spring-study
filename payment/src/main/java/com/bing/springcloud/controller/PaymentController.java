adc12881922a94cf32f7f8775be1ff27661df3e5
bing.zhang <Bing.zhang@trjcn.com>
2020-06-19 17:58:10 +0800

    eureka yml 配置

--- payment/src/main/java/com/bing/springcloud/controller/PaymentController.java
+++ payment/src/main/java/com/bing/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.bing.springcloud.entity.Payment;
 import com.bing.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author bing.zhang
@@ -23,6 +26,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String port;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping("payment/create")
     public CommonResult create(@RequestBody Payment payment) {
@@ -44,5 +50,18 @@ public class PaymentController {
         } else {
             return new CommonResult(444, "没有对应记录，查询ID: " + id);
         }
+    }
+
+    @GetMapping("payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("*****service:" + service);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
